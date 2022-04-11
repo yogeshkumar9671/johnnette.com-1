9868f037a14c7422ce6cfcb3c32474c4ef561d31
johnnette365 <103301325+johnnette365@users.noreply.github.com>
2022-04-11 12:13:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,18 +10,20 @@ menu.onclick = () => {
     icons.classList.toggle('active')
 }
 
-let button1 = document.querySelector('#button1')
-let submenu1 = document.querySelector('#sub-menu-1')
-button1.onclick = () => {
-    button1.classList.toggle('fa-angle-double-up')
-    submenu1.classList.toggle('active')
-}
-let button2 = document.querySelector('#button2')
-let submenu2 = document.querySelector('#sub-menu-2')
-button2.onclick = () => {
-    button2.classList.toggle('fa-angle-double-up')
-    submenu2.classList.toggle('active')
-}
+// let button1 = document.querySelector('#button1')
+// let submenu1 = document.querySelector('#sub-menu-1')
+// button1.onclick = () => {
+//     button1.classList.toggle('fa-angle-double-up')
+//     submenu1.classList.toggle('active')
+// }
+
+// let button2 = document.querySelector('#button2')
+// let submenu2 = document.querySelector('#sub-menu-2')
+// button2.onclick = () => {
+//     button2.classList.toggle('fa-angle-double-up')
+//     submenu2.classList.toggle('active')
+// }
+
 let button3 = document.querySelector('#button3')
 let submenu3 = document.querySelector('#sub-menu-3')
 button3.onclick = () => {
